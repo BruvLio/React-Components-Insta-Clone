2c831d7666149643873073356e61e5780ba8004e
Lionel Mbayu <Lionel.Mbayu@gmail.com>
2022-09-12 09:41:30 -0500

    Final Commit

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -8,6 +8,12 @@ const Comments = props => {
 
   return (
     <div>
+      {
+        comments.map(comment => {
+          return <Comment/>
+        } 
+        )
+      }
       {/* map through the comments prop and render a Comment for every piece of data */}
     </div>
   );
